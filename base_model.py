252006515e1d1ea947ca1992b233d0bb702ac626
papaseyone <charleskumahntim@gmail.com>
2024-02-12 05:03:09 -0800

    updated

---
+++ base_model.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""the Model Base"""
+import uuid
+from datetime import datetime
+import models
+
+
+class BaseModel:
+    """the class base"""
+    def __init__(self, *args, **kwargs):
+         """ the Constructor """
+        if kwargs:
+
+            for key, value in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
+    def __str__(self):
+        """String representation of object"""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
+    def save(self):
+        """Update and save object"""
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """Convert object to dictionary"""
+        dict_copy = self.__dict__.copy()
+        dict_copy["created_at"] = self.created_at.isoformat()
+        dict_copy["updated_at"] = self.updated_at.isoformat()
+        dict_copy['__class__'] = self.__class__.__name__
+        return dict_copy
+
